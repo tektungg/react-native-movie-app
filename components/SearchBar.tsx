089ca8476ab8267effb5acf527de0cf9cca52fdc
Rama Pramudhita Bhaskara <ramapramudhita@venturo.pro>
2026-01-29 13:42:08 +0700

    feat: implement SearchBar component and update main screen layout

---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor={"#AB8BFF"}
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        placeholderTextColor={"#A8b5DB"}
+        value=""
+        onChangeText={() => {}}
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
